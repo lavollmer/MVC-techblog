812841f3f393d8cdcc5a31d672ea2b2538cae43f
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-11-12 18:15:10 -0600

    setup for files

--- server.js
+++ server.js
@@ -1,32 +1,43 @@
 //requiring in express
 const express = require("express");
+const router = express.Router();
+
+// Sets up the Express App
 const app = express();
-const routes = require("./controllers");
-const path = require("path")
-//import sequelize
+const path = require('path');
+
+//requiriing in routes (controllers)
+const routes = require("./controllers/index");
+
+//setting up handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//import sequelize with connection db
 const sequelize = require('./config/connection');
 
-//name the PORT
+//name the PORT & set up for heroku to deploy
 const PORT = process.env.PORT || 3001;
 
-//public folder is available 
+//public folder is available and middleware
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Home page route
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"))
+router.get("/", async (req, res) => {
+  res.render('all');
 })
 
 //after we have defined this route, any other routes that hit this file will be divided up
 app.use(routes)
 
-//safeguard: if we are in a production environment it should always equal false
-const okToSync = (process.env.NODE_ENV === 'product') ? false : true;
-
 // Connect to the database before starting the Express.js server
 //force:okToSync telling sequelize to rebuild the tables
-sequelize.sync({ force: okToSync }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+
