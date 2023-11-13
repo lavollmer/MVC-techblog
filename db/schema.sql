7632d13763514a87a72549f6cf295cb90f0bbcf6
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-11-12 19:48:28 -0600

    working on associations

--- db/schema.sql
+++ db/schema.sql
@@ -17,12 +17,16 @@ CREATE TABLE User (
 -- Create article Table--
 CREATE TABLE Article (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  article_user VARCHAR(500) NOT NULL
+  article_user VARCHAR(500) NOT NULL,
+  article_id INT,
+  FOREIGN KEY (article_id) REFERENCES User(id)
 );
 
 -- Create comment Table--
 CREATE TABLE Comment (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  comment_user VARCHAR(500) NOT NULL
+  comment_user VARCHAR(500) NOT NULL,
+  comment_id INT,
+    FOREIGN KEY (comment_id) REFERENCES User(id)
 );
 
---
+++ models/index.js
@@ -0,0 +1,26 @@
+const User = require('./user');
+const Article = require('./article');
+const Comment = require('./comment')
+
+//user has many articles, but article has one user
+User.hasMany(Article, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Article.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+//user has many comments, but a made comment has one user
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+
+module.exports = { User, Article, Comment };
