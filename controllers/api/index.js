eeca1eca5a4012b95cee6a4cfcc1797a25adf6e9
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-11-13 18:11:16 -0600

    debugging

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const router = require('express').Router;
+//const express = require('express');
+const router = require('express').Router();
 
 //controller files
 const userRoutes = require('./user-routes')
